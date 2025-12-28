b9bb532238d91a7a90124304996df50c5ffc0491
[dyad] <luisvdelfin@gmail.com>
2025-12-28 14:51:35 -0300

    [dyad] Fixed Layout component props error - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-const Layout = ({ LayoutProps }: LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen bg-black">
       <Navbar />
